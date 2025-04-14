b425402942f62ca07a77ee37442690ec22514704
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-14 19:37:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
 from aiogram import Bot, Dispatcher
-bot = Bot(token='7853195961:AAHFMQm9fbvVNXvDbq2Kv192_HvOxTK0gHY')
-dp = Dispatcher(bot)
+from config import BOT_TOKEN
+bot = Bot(token='7853195961:AAHFMQm9fbvVNXvDbq2Kv192_HvOxTK0gHY' parse_mode="HTML")
+dp = Dispatcher()
