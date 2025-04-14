7ef183035b5b4d464e519039229543fa1614e1d3
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-14 19:38:33 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,9 @@
-# APScheduler setup
+# APScheduler setupfrom apscheduler.schedulers.asyncio import AsyncIOScheduler
+from services.news import post_latest_news
+from services.shikimori import post_today_schedule
+
+def setup_scheduler(bot, dispatcher):
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(post_latest_news, "interval", hours=1, args=[bot])
+    scheduler.add_job(post_today_schedule, "cron", hour=7, minute=0, args=[bot])
+    scheduler.start()
