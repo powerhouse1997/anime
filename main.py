beac6a4f4003adbc327677633c986604a2352cea
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-14 19:38:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Start the bot')
+from aiogram import F
+from aiogram.enums import ParseMode
+from aiogram import Dispatcher
+from aiogram.types import Message
+from aiogram import Bot
+from aiogram.client.session.aiohttp import AiohttpSession
+from config import BOT_TOKEN
+from handlers import anime, news, schedule
+from scheduler import setup_scheduler
+
+async def on_startup(bot: Bot, dispatcher: Dispatcher):
+    setup_scheduler(bot, dispatcher)
+
+async def main():
+    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML, session=AiohttpSession())
+    dp = Dispatcher()
+
+    anime.register(dp)
+    news.register(dp)
+    schedule.register(dp)
+
+    await on_startup(bot, dp)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
