3db15dc8313338f6a92b9f608d26b05cec6ce996
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-15 21:53:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 from aiogram import Bot
 
 async def run():
-    bot = Bot(token="YOUR_BOT_TOKEN")
+    bot = Bot(token="TELEGRAM_BOT_TOKEN")
     await bot.delete_webhook(drop_pending_updates=True)
     print("Webhook deleted.")
     await bot.session.close()
