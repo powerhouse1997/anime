c8135081035b3f667e8011221ef5600f6ec45c3a
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-19 17:17:12 +0530

    Update sheduler.py

--- sheduler.py
+++ sheduler.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 from datetime import datetime, timedelta
 from aiogram import Bot
-from aiogram.emu import ParseMode
+from aiogram.types import ParseMode
 
 # Anilist API URL and Query to get currently airing anime
 ANILIST_API_URL = "https://graphql.anilist.co"
